d8e5d84b250037ca8a896af5ba14e22b9901ef87
thdcodud01 <thdcodud01@gmail.com>
2023-03-08 12:27:46 +0900

    examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 프로그램의 시작점
+         // System.out.println -> 출력 명령어 (=console.log("")) & . 은 ~의 와 같은 의미
+        int age = 50;
+        System.out.println("당신의 나이 : " + age);
+
+        if (age>=20 && age<=34) {
+            System.out.printf("청년입니다.");
+        } else {
+            System.out.printf("청년이아닙니다.");
+        }
+        // 프로그램의 끝점
     }
 }
